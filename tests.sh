4be52cc9d80186cee12d6c4219dc66e649dcf66f
Alexey <alexeyvoytenko.2000@gmail.com>
2021-10-01 03:31:28 +1000

    added screenshots

--- tests.sh
+++ tests.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
-url=http://localhost/restapi/public/index.php
+url=https://restapi.alexvoyt.com
 
 SendRequest()
 {
@@ -7,16 +7,23 @@ SendRequest()
     route=$2
     json=$3
 
+    printf "Curl request: ";
+    echo curl -X $method $url$route\
+         -H 'Content-Type: application/json'\
+         -d $json
+
+    printf "Response: ";
     curl -X $method $url$route\
          -H 'Content-Type: application/json'\
          -d $json
-    printf "\n";
+    printf "\n\n";
 }
 
 SendRequest POST /user/ '{"login":"user","password":"user"}'
 SendRequest POST /user/ '{"login":"user2","password":"user2"}'
+SendRequest POST /user/ '{"login":"user3","password":"user3"}'
 SendRequest GET /todo/ '{"login":"user","password":"user"}'
-SendRequest DELETE /todo/1 '{"login":"user2","password":"user2"}'
+SendRequest PUT /todo/2 '{"login":"user","password":"user","description":"updated_description_2"}'
+SendRequest POST /todo/ '{"login":"user","password":"user","description":"Haha_todo_thingy_goes_brrrrr2"}'
 SendRequest DELETE /todo/1 '{"login":"user","password":"user"}'
-SendRequest PUT /todo/5 '{"login":"user","password":"user","description":"updated_description_2"}'
-# SendRequest POST /todo/ '{"login":"user","password":"user","description":"Haha todo thingy goes brrrrr"}'
+SendRequest DELETE /todo/132 '{"login":"user","password":"user"}'
