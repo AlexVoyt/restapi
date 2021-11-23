626091b987a264acbb38fcaf544ae21cc5ba8b4a
Alexey <alexeyvoytenko.2000@gmail.com>
2021-11-24 01:02:53 +1000

    Delete file endpoint

--- src/Controller/FileController.php
+++ src/Controller/FileController.php
@@ -12,6 +12,8 @@ use Symfony\Component\HttpFoundation\BinaryFileResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Filesystem\Filesystem;
+use Symfony\Component\Filesystem\Exception\IOExceptionInterface;
 
 // NOTE: for this set of endpoints, we provide login and passwords in headers,
 // not in json
@@ -174,4 +176,59 @@ class FileController extends AbstractController
             ]);
         }
     }
+
+    #[Route('/{originalName}', name: '.delete', methods: ["DELETE"])]
+    public function deleteFile(Request $req, UserRepository $user_rep, 
+                               FileRepository $file_rep, $originalName): Response
+    {
+        $login = $req->headers->get("REST-Login");
+        $password = $req->headers->get("REST-Password");
+
+        $user = $user_rep->getUser($login, $password);
+        if($user)
+        {
+            $file = $file_rep->findOneBy([
+                "owner" => $user->getId(),
+                "originalName" => $originalName
+            ]);
+
+            if($file)
+            {
+                $em = $this->getDoctrine()->getManager();
+                $em->remove($file);
+                $em->flush();
+
+                $filesystem = new Filesystem();
+                try {
+                    $filesystem->remove([$this->getParameter('uploads_dir') . '/' . $file->getSafeName()]);
+                } catch (IOExceptionInterface $e) {
+                    return $this->json([
+                        'status' => 400,
+                        'message' => 'Exception occured during file deletion',
+                        'exception message' => $e->getMessage()
+                    ]);
+                }
+
+                return $this->json([
+                    'status' => 200,
+                    'message' => "File succesfully deleted"
+                ]);
+            }
+            else
+            {
+                return $this->json([
+                    'status' => 400,
+                    'message' => "File not found"
+                ]);
+            }
+        }
+        else
+        {
+            return $this->json([
+                'status' => 400,
+                'message' => "User not found"
+            ]);
+        }
+    }
+
 }
